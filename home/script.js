b5f1f8ec6ac04edce32e9f2d6929fa14774cb3e3
Anas Ali <mynaameis321@gmail.com>
2025-02-01 16:33:06 +0500

    task completed

---
+++ home/script.js
@@ -0,0 +1,6 @@
+document.getElementById('login').addEventListener('click', () => {
+  window.location.href = 'login.html';
+});
+document.getElementById('Register').addEventListener('click', () => {
+  window.location.href = 'register.html';
+});
